8e557154c73cf08cf38edfae7ed2674e08b8003d
zz12343 <128247308+zz12343@users.noreply.github.com>
2025-10-24 20:29:14 -0700

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,7 +18,7 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_1: '分享穿搭',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
   HEO_HERO_TITLE_4: '新版上线',
