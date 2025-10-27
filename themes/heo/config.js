222080a50bea482fa23808b6449c0fe03794f27a
zz12343 <128247308+zz12343@users.noreply.github.com>
2025-10-26 17:30:27 -0700

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客' },
+    { title: '欢迎来到我的博客',url：'https://www.isinna.blog/article/29748ec1-6aa1-8013-87aa-e6ee29873cb4 },
+    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
