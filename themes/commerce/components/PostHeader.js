db3bd0cb3f6c8901625431281a15a082ec030323
zz12343 <128247308+zz12343@users.noreply.github.com>
2025-11-27 03:57:25 +0800

    Update PostHeader.js

--- themes/commerce/components/PostHeader.js
+++ themes/commerce/components/PostHeader.js
@@ -10,11 +10,11 @@ export default function PostHeader({ post }) {
   const headerImage = post?.pageCover ? post.pageCover : siteConfig('HOME_BANNER_IMAGE')
 
   return (
-    <div id="header" className="w-full h-60 relative md:flex-shrink-0 z-10" >
+    <div id="header" className="w-full h-90 relative md:flex-shrink-0 z-10" >
       <LazyImage priority={true} src={headerImage} className='w-full h-full object-cover object-center absolute top-0'/>
 
       <header id='article-header-cover'
-            className="bg-black bg-opacity-70 absolute top-0 w-full h-60 py-10 flex justify-center items-center ">
+            className="bg-black bg-opacity-70 absolute top-0 w-full h-90 py-10 flex justify-center items-center ">
 
         <div className='mt-10'>
             <div className='mb-3 flex justify-center'>
