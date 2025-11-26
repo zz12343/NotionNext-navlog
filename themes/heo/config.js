ff5e6e99817f981a9d646f970749f0f60468936f
zz12343 <128247308+zz12343@users.noreply.github.com>
2025-11-27 03:16:29 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -39,7 +39,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    '你好！我是nana',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
@@ -120,7 +120,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://isinna.blog',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
